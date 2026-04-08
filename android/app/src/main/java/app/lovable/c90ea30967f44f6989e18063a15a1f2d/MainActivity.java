0684fbb71495a2412ede91592210c8a3774342b1
karankumar3932 <karankumar3932@gmail.com>
2026-04-09 01:44:20 +0530

    Updated with correct GitHub account

--- android/app/src/main/java/app/lovable/c90ea30967f44f6989e18063a15a1f2d/MainActivity.java
+++ android/app/src/main/java/app/lovable/c90ea30967f44f6989e18063a15a1f2d/MainActivity.java
@@ -2,4 +2,5 @@ package app.lovable.c90ea30967f44f6989e18063a15a1f2d;
 
 import com.getcapacitor.BridgeActivity;
 // updated by karan
+// final update
 public class MainActivity extends BridgeActivity {}
